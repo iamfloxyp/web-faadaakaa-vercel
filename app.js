212dde9e3dbffb4138622737b704982c508e9a8a
rabiu_niyi <niyious@gmail.com>
2025-12-16 14:42:20 +0100

    cleaned version

--- app.js
+++ app.js
@@ -1,29 +1,34 @@
-import express from 'express';
-import path from 'path';
-
-const __dirname = path.dirname(new URL(import.meta.url).pathname);
+const express = require('express');
 const app = express();
+const PORT = 3000;
 
-// homepage 
+const path = require('path');
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define a simple GET route for the homepage
 app.get('/', (req, res) => {
-  res.sendFile(Path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+//'/users/:userId/books/:bookId'
+app.get('/category', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'products_list.html'));
 });
 
-// cart page
-app.get('/cart', (req, res) => {
-  res.sendFile(Path.join(__dirname, 'cart.html'));
+app.get('/category/{:category_slug}/{:page}', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'products_list.html'));
 });
 
-// signUp page
-app.get('/signUp', (req, res) => {
-  res.sendFile(Path.join(__dirname, 'signUp.html'));
-});
-// Login page
-app.get('/login', (req, res) => {
-  res.sendFile(Path.join(__dirname, 'login.html'));
+// Define another GET route for an 'about' page
+app.get('/about', (req, res) => {
+  res.send('Welcome to the about us page.'); // Sends a different response
 });
 
+app.get('/coming', (req, res) => {
+  res.send('Welcome to the coming page.'); // Sends a different response
+});
 
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
-
-const PORT = process.env.PORT || 3000;
+// Start the server and listen on the specified port
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
